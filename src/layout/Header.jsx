e98bb6a927adf74533b2d86fa382dbd776486619
Hrant <hrantalexanyan13@gmail.com>
2021-12-29 21:15:23 +0400

    done

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,17 +2,12 @@ function Header() {
     return (
         <nav className="deep-purple accent-2">
             <div className="nav-wrapper">
-                <a
-                    href="https://hrant01.github.io/react-movies-project/"
-                    className="brand-logo"
-                >
+                <a href="!#" className="brand-logo">
                     React Movies
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li>
-                        <a href="https://github.com/HRANT01/react-movies-project">
-                            Repo
-                        </a>
+                        <a href="!#">Repo</a>
                     </li>
                 </ul>
             </div>
